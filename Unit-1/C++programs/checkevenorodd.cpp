2173487042b8b0dc4dcd99e2fbcac6edd093fe71
bhanu <bhanubethala2@gmail.com>
2023-01-08 21:20:55 +0530

    day-15
    check whether given number is odd or even

---
+++ Unit-1/C++programs/checkevenorodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    cout<<"enter n value";
+    cin>>n;
+    if(n%2==0)
+    cout<<n<<"given number is even\n";
+    else
+    cout<<n<<"given number is odd\n";
+}
