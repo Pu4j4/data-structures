2518f899fe7ffe2aa5d19c9938cd392f5c0a5dbc
bhanu <bhanubethala2@gmail.com>
2023-01-08 21:20:11 +0530

    day-15
    check whether given number is positive or negative

---
+++ Unit-1/C++programs/check+veor-venum.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    clrscr();
+    cout<<"enter n value";
+    cin>>n;
+    if(n>0)
+    cout<<n<<"given number is positive\n";
+    else
+    cout<<n<<"given number is negative\n";
+}
