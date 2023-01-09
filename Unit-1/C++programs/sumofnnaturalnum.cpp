585a6eeb8565f2ff28caa52f1005f2436fc9c41d
bhanu <bhanubethala2@gmail.com>
2023-01-09 22:48:37 +0530

    day-16
    To find sum of n natural num is
    using functions

---
+++ Unit-1/C++programs/sumofnnaturalnum.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+int naturalsum(int n);
+void main()
+{
+    int n;
+    clrscr();
+    cout<<"enter n value"
+    cin>>n;
+    cout<<"sum of"<<n<<"natural number is"<<naturalsum(n)<<endl;
+}
+int naturalsum(int n)
+{
+    int i,s=0;
+    for(i=0;i<=n;i++)
+    sum=sum+1;
+    return sum;
+}
