fc6cee8eabd359210b3eb32d0e147604e9bace8f
bhanu <bhanubethala2@gmail.com>
2023-01-09 22:47:06 +0530

    day-16
    check whether given num is prime or not

---
+++ Unit-1/C++programs/givennumprimeornot.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+int prime(int);
+void main()
+{
+    int n;
+    clrscr();
+    cout<<"enter n value";
+    cin>>n;
+}
+int prime(int n)
+{
+int n;
+for(i=0;i<=0;i++)
+if(n%i==0)
+return 0;
+return 1;
+}
+{
+if prime(n)==1
+cout<<n<<"is prime";
+else
+cout<<n<<"is not prime";
+}
