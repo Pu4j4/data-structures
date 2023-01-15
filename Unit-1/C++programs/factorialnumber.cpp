bbbb458f34c667325ec7cc76c74845b8dfc7e210
bhanu <bhanubethala2@gmail.com>
2023-01-15 21:59:06 +0530

    day-21
    To find factorial of a given number

---
+++ Unit-1/C++programs/factorialnumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+using namespace std;
+int main()
+ {
+    int n;
+    long fact = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else 
+    {
+        for(int i= 1; i<= n; ++i)
+         {
+            fact *= i;
+        }
+        cout << "Factorial of " << n << " = " << fact;    
+    }
+
+    return 0;
+}
