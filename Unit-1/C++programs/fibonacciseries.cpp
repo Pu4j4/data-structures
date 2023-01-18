0d0a8f6e10bd57f18a647a18d0ba0ea70b62e6c5
bhanu <bhanubethala2@gmail.com>
2023-01-19 00:02:11 +0530

    day-21

--- Unit-1/C++programs/fibonacciseries.cpp
+++ Unit-1/C++programs/fibonacciseries.cpp
@@ -4,8 +4,8 @@
       int n1=0,n2=1,n3,i,number;    
      cout<<"Enter the number of elements: ";    
      cin>>number;    
-     cout<<n1<<" "<<n2<<" "; //printing 0 and 1    
-     for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+     cout<<n1<<" "<<n2<<" ";
+     for(i=2;i<number;++i)
      {    
       n3=n1+n2;    
       cout<<n3<<" ";    
