4692abd576f573f8f88c9cb94ab84deaa3a9b58f
bhanu <bhanubethala2@gmail.com>
2023-02-12 21:59:24 +0530

    day-36
    check whether leap year or not

---
+++ Unit-1/checkleapyear.cpp
@@ -0,0 +1,21 @@
+#include 
+using namespace std;
+bool checkleapyear(int year) 
+{ 
+    if (year % 400 == 0) 
+        return true; 
+
+    if (year % 100 == 0) 
+        return false; 
+
+    if (year % 4 == 0) 
+        return true; 
+    return false; 
+} 
+
+int main() {
+ int yr = 2020;
+ if(checkleapyear(yr))cout<<"Leap Year";
+ else cout<<"Not Leap Year";
+return 0;
+}
