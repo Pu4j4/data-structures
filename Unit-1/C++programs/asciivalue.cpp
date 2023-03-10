6997e68670b1d32dd850f40fad4555e2d9a41c41
bhanu <bhanubethala2@gmail.com>
2023-03-10 19:02:08 +0530

    day-48
    to display ascii value of character

---
+++ Unit-1/C++programs/asciivalue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     char c;
+
+     cout << "Enter any Character :: ";
+     cin >> c;
+
+     cout << "\nThe ASCII Value of Character [ "<< c << " ] is :: " << int(c)<<"\n";
+
+     return 0;
+}
