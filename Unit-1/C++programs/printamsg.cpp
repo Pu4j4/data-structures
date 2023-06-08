1bff6d03aad5a0306fcf6fbd37d36f63c05362ec
bhanu <bhanubethala2@gmail.com>
2023-06-08 23:25:57 +0530

    day-80
    to print a message multiple times

---
+++ Unit-1/C++programs/printamsg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,i;
+  string str;
+  
+ cout<<"Enter The MSG :\n";
+ cin>>str;
+  
+ cout<<"Enter How Many Times You Want To Print MSG : \n";
+ cin>>n;
+  
+ for(i=0 ;i<n ;i++)
+ {
+   cout<<str<<endl;
+ } 
+ cout<<endl;
+ return 0;
+
+}
